39c010968f78aae4a889206381cd28956cc43a87
ansuleymanova <suleymanova.dev@gmail.com>
2022-08-23 18:42:30 +0300

    adapt Menu for mobile

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,15 +3,15 @@ export default function Menu () {
         <menu className="sidebar__menu">
             <a className="link" target="_blank" href="https://github.com/ansuleymanova/variance-my-beloved">
                 <div className="icon icon_type_sigma"></div>
-                VMB на Github
+                <p className="link__text">VMB на Github</p>
             </a>
             <a className="link" target="_blank" href="https://github.com/ansuleymanova">
                 <div className="icon icon_type_github"></div>
-                Я на Github
+                <p className="link__text">Я на Github</p>
             </a>
             <a className="link" target="_blank" href="https://scholar.google.com/citations?&user=IxNO5GEAAAAJ">
                 <div className="icon icon_type_gs"></div>
-                Я в Google Scholar
+                <p className="link__text">Я в Google Scholar</p>
             </a>
         </menu>
     )
