e2d2f3ea1d9b234eb750b4a9777c09046b2673b4
ansuleymanova <suleymanova.dev@gmail.com>
2022-08-23 18:05:13 +0300

    add RightSidebar menu links & functionality

---
+++ src/components/Menu.tsx
@@ -0,0 +1,18 @@
+export default function Menu () {
+    return (
+        <menu className="sidebar__menu">
+            <a className="link" target="_blank" href="https://github.com/ansuleymanova/variance-my-beloved">
+                <div className="icon icon_type_sigma"></div>
+                VMB на Github
+            </a>
+            <a className="link" target="_blank" href="https://github.com/ansuleymanova">
+                <div className="icon icon_type_github"></div>
+                Я на Github
+            </a>
+            <a className="link" target="_blank" href="https://scholar.google.com/citations?&user=IxNO5GEAAAAJ">
+                <div className="icon icon_type_gs"></div>
+                Я в Google Scholar
+            </a>
+        </menu>
+    )
+}
--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,7 +1,24 @@
-export default function RightSidebar (props: any) {
+import { useState } from 'react';
+import Menu from './Menu.js';
+
+export default function RightSidebar () {
+    const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState<boolean>(false);
+    let sidebarClassName = `sidebar sidebar_type_right ${isBurgerMenuOpen ? "sidebar_open" : ""}`;
+
+    function onBurgerMenuClick() {
+        setIsBurgerMenuOpen(!isBurgerMenuOpen);
+    }
+
+    function onSidebarClick() {
+        if (isBurgerMenuOpen) {
+            setIsBurgerMenuOpen(false);
+        }
+    }
+
     return(
-        <div className="sidebar sidebar_type_right">
-            <button className="sidebar__menu-button"></button>
+        <div className={sidebarClassName} onClick={onSidebarClick}>
+            <button className="sidebar__menu-button" onClick={onBurgerMenuClick}></button>
+            {isBurgerMenuOpen ? <Menu /> : ""}
         </div>
     )
 }
